5cf27d49a80057debb037715568d2c430db07964
anthony immenschuh <60157977+anthonyimmen@users.noreply.github.com>
2025-03-30 16:49:18 -0500

    Time: 11 ms (72.52%) Space: 18 MB (33.80%)

---
+++ 0242-valid-anagram/valid-anagram.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        # use a dictionary with letter and occurences compare if they are identical
+
+        sDict = defaultdict(int)
+        tDict = defaultdict(int)
+
+        for letter in s:
+            sDict[letter] += 1
+        
+        for letter in t:
+            tDict[letter] += 1
+
+        return sDict == tDict
+        
