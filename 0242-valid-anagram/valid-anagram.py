315162d54041991f76b883f1cada6424ad6ac5e1
anthony immenschuh <60157977+anthonyimmen@users.noreply.github.com>
2025-03-30 16:52:05 -0500

    Time: 11 ms (72.52%) Space: 17.8 MB (64.46%)

--- 0242-valid-anagram/valid-anagram.py
+++ 0242-valid-anagram/valid-anagram.py
@@ -1,6 +1,7 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         # use a dictionary with letter and occurences compare if they are identical
+        # use default dict to set the default type value for the keys
 
         sDict = defaultdict(int)
         tDict = defaultdict(int)
