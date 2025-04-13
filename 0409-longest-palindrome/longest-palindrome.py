5e07e266c1c5da8b010f3e200efb73690c46dd4e
anthony immenschuh <60157977+anthonyimmen@users.noreply.github.com>
2025-04-13 15:26:12 -0500

    Time: 0 ms (100.00%) Space: 18 MB (7.06%)

---
+++ 0409-longest-palindrome/longest-palindrome.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        letters = defaultdict(int)
+
+        for letter in s:
+            letters[letter] += 1
+
+        print(letters)
+        
+        longest = 0
+        for key, value in letters.items():
+            if value % 2 == 0:
+                longest += value
+            else:
+                longest += value - 1
+
+        if longest < len(s):
+            longest += 1
+            
+        return longest
+        
