416f8b331525fe10530ef305d14d6cfd8042bdbc
anthony immenschuh <60157977+anthonyimmen@users.noreply.github.com>
2025-04-13 15:31:32 -0500

    Time: 3 ms (42.32%) Space: 17.9 MB (47.05%)

--- 0409-longest-palindrome/longest-palindrome.py
+++ 0409-longest-palindrome/longest-palindrome.py
@@ -2,6 +2,7 @@ class Solution:
     def longestPalindrome(self, s: str) -> int:
         letters = defaultdict(int)
 
+        # create the hash map
         for letter in s:
             letters[letter] += 1
 
@@ -14,8 +15,9 @@ class Solution:
             else:
                 longest += value - 1
 
+        # essentially means that it had an odd value
         if longest < len(s):
             longest += 1
-            
+
         return longest
         
