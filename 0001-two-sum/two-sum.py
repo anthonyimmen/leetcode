4edd7424819b78ef80777151840d83f6fb1d0080
anthony immenschuh <60157977+anthonyimmen@users.noreply.github.com>
2025-04-23 17:19:39 -0500

    Time: 0 ms (100.00%) Space: 18.8 MB (63.11%)

--- 0001-two-sum/two-sum.py
+++ 0001-two-sum/two-sum.py
@@ -1,11 +1,17 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        exists = defaultdict(int)
+        # brute force: nested for loop comparing every number with each other
+        # to see if it will add up to the target. T=o(n^2) S=o(1)
 
+        # efficient: use a dictionary, if target-curr in dictionary and curr + dict val = target
+        # return, else insert that value into the dictionary
+
+        numDict = defaultdict(int)
+        
         for i in range(len(nums)):
-            diff = target - nums[i]
-            if diff in exists:
-                return [i, exists[diff]]
+            if target-nums[i] in numDict.keys():
+                return [i, numDict[target-nums[i]]]
             else:
-                exists[nums[i]] = i
+                numDict[nums[i]] = i
+
         
