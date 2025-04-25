b6fc01bc8149b11f588c3bddfdfcb6933cc3a917
anthony immenschuh <60157977+anthonyimmen@users.noreply.github.com>
2025-04-24 20:07:04 -0500

    Time: 135 ms (21.80%) Space: 27 MB (33.23%)

--- 0121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,15 +1,16 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # brute force - calculate every price combination using a nested for loop
-        # more efficient - if next day price is less than current day, make that the new
+        # brute force is to calculate the price of each combination to buy and sell the stock
 
-        maxProfit = 0
-        startingDay = 0
-        currDay = 1
+        lowest = prices[0]
+        profit = 0
 
-        while currDay < len(prices):
-            maxProfit = max(maxProfit, prices[currDay] - prices[startingDay])
-            if prices[currDay] < prices[startingDay]:
-                startingDay = currDay
-            currDay += 1
-        return maxProfit
+        for i in range(len(prices)):
+            lowest = min(prices[i], lowest)
+            if prices[i] > lowest:
+                profit = max(profit, prices[i] - lowest)
+
+        return profit
+
+
+
