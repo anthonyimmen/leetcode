54ea8a99252552e21bfaf70ea14062d195165695
anthony immenschuh <60157977+anthonyimmen@users.noreply.github.com>
2025-04-27 15:46:17 -0500

    Time: 37 ms (17.22%) Space: 18.2 MB (12.54%)

---
+++ 0003-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -0,0 +1,29 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        # kadane's algorithm?
+        maxSum = 0
+
+        # keep's track of the index where it occured at
+        appeared = defaultdict(int)
+        left = 0
+
+        for right in range(len(s)):
+            print(right-left + 1)
+            if s[right] in appeared and appeared[s[right]] >= left:
+                left = appeared[s[right]] + 1
+                appeared[s[right]] = right
+            else:
+                appeared[s[right]] = right
+                maxSum = max(maxSum, right-left + 1)
+        
+        if len(s):
+            return maxSum
+        else:
+            return 0
+            
+
+
+
+
+
+        
